14b3fe157a982ec4cfeae6ef933dbc80774a4a3d
dogukanakkaya <doguakkaya27@hotmail.com>
2023-08-01 02:39:27 +0300

    feat: add verify command & chore: renames in upload script

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,9 +2,19 @@ import 'dotenv/config'
 import "tsconfig-paths/register";
 import "@nomicfoundation/hardhat-toolbox";
 import { HardhatUserConfig } from "hardhat/config";
+import { POLYGONSCAN_API_KEY, POLYGON_MUMBAI_RPC_PROVIDER } from './config';
 
 const config: HardhatUserConfig = {
   solidity: "0.8.19",
+  networks: {
+    mumbai: {
+      accounts: [],
+      url: POLYGON_MUMBAI_RPC_PROVIDER
+    }
+  },
+  etherscan: {
+    apiKey: POLYGONSCAN_API_KEY
+  }
 };
 
 export default config;
